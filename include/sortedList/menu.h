f133ab460f093dae0462614c70295f93c9a164aa
MichaelVasiliev24 <mv82374@gmail.com>
2025-12-26 00:15:50 +0300

    Fix: Change header file for menu functions

--- include/sortedList/menu.h
+++ include/sortedList/menu.h
@@ -2,8 +2,5 @@
 #include "sortedList.h"
 
 void clearInputBuffer(void);
-int readInt(const char* prompt, int* value);
 void printMenu(void);
-void handleInsert(SortedList* list);
-void handleDelete(SortedList* list);
 int processChoice(int choice, SortedList* list);
