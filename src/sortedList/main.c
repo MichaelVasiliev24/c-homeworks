b8c054077f3adcc7a2bf31db5cba4fa9bb4ed0cb
MichaelVasiliev24 <mv82374@gmail.com>
2025-12-25 17:55:01 +0300

    Update main.c to support the --test flag for tests

--- src/sortedList/main.c
+++ src/sortedList/main.c
@@ -1,10 +1,29 @@
 #include "../../include/sortedList/menu.h"
 #include "../../include/sortedList/sortedList.h"
+#include "../../include/sortedList/testSortedList.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    // Проверка флага --test для запуска тестов
+    if (argc > 1 && strcmp(argv[1], "--test") == 0) {
+        printf("Running tests for sortedList...\n");
+        printf("==============================\n");
+        runAllTests();
+        int result = runTestsWithSummary();
+        printf("==============================\n");
+        if (result == 0) {
+            printf("All tests passed!\n");
+        }
+        else {
+            printf("Some tests failed!\n");
+        }
+        return result;
+    }
+
+    // Обычный режим работы программы
     SortedList* list = createSortedList();
     if (list == NULL) {
         printf("Ошибка создания списка\n");
@@ -12,7 +31,7 @@ int main(void)
     }
 
     int choice;
-    int shouldExit = 0;
+    bool shouldExit = false;
 
     do {
         printMenu();
